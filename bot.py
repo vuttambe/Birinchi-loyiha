870031a78aebb27336052c63ac33d554b86033d8
vuttambe <vuttambe@gmail.com>
2026-04-16 21:24:45 +0500

    removed

--- bot.py
+++
@@ -1,9 +0,0 @@
-import telebot
-
-bot = telebot.TeleBot("8709262637:AAFLgpQPqkJIDf8JVD6BhUbvJvuVQ_XHbmU")
-
-@bot.message_handler(func=lambda m: True)
-def javob(message):
-    bot.reply_to(message, "Salom! Xabaringizni oldim: " + message.text)
-
-bot.polling()
